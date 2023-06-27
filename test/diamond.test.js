d26ab0e326b284b6e65fd60da566777363c96306
Joshua Hebert <jhebertcocktails@gmail.com>
2023-06-27 00:33:44 -0400

    fixed diamond.js added heart.js code

--- test/diamond.test.js
+++ test/diamond.test.js
@@ -1,14 +1,14 @@
 const Diamond = require("../lib/diamond");
 
 describe('Diamond', () => {
-    describe("render Method", () => {
+    describe("Render Method", () => {
     it('should render a Diamond', () => {
         logoName = "FZZ",
         logoColor = "red",
         textColor = "white"
         logoShape = "diamond"
-        const circle = new Diamond(logoName, logoColor, textColor);
-        expect(circle.render()).toBe(`
+        const diamond = new Diamond(logoName, logoColor, textColor);
+        expect(diamond.render()).toBe(`
         <div style="background-color: ${this.logoColor}; height: 200px; width: 200px; border-radius: 50%; display: flex; justify-content: center; align-items: center;">
             <h1 style="color: ${this.textColor}; font-size: 100px;">${this.logoName}</h1>
         </div>
--- test/heart.test.js
+++ test/heart.test.js
@@ -1,14 +1,14 @@
-const Diamond = require("../lib/diamond");
+const Heart = require("../lib/heart");
 
-describe('Diamond', () => {
-    describe("render Method", () => {
-    it('should render a Diamond', () => {
+describe('Heart', () => {
+    describe("Render Method", () => {
+    it('should render a Heart', () => {
         logoName = "FZZ",
         logoColor = "red",
         textColor = "white"
-        logoShape = "diamond"
-        const circle = new Diamond(logoName, logoColor, textColor);
-        expect(circle.render()).toBe(`
+        logoShape = "heart"
+        const heart = new Heart(logoName, logoColor, textColor);
+        expect(heart.render()).toBe(`
         <div style="background-color: ${this.logoColor}; height: 200px; width: 200px; border-radius: 50%; display: flex; justify-content: center; align-items: center;">
             <h1 style="color: ${this.textColor}; font-size: 100px;">${this.logoName}</h1>
         </div>
