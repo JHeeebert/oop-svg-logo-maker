38422ffcf8263ab15c0d47caf051d89aedc794db
Joshua Hebert <jhebertcocktails@gmail.com>
2023-06-28 22:55:52 -0400

    debig finally

--- index.js
+++ index.js
@@ -2,91 +2,176 @@
 const inquirer = require('inquirer');
 // imports node file system
 const fs = require('fs');
-// imports classes from lib folder
-const { generateSVG } = require('./lib/generateSVG');
-const { generateShapes } = require('./lib/generateShapes');
 
-class Svg{ 
-    constructor(logoName, logoColor, logoTextColor, logoShape) {
-        this.logoName = logoName;
-        this.logoColor = logoColor;
-        this.logoTextColor = logoTextColor;
-        this.logoShape = logoShape;
-    }
-    generateSvg() {
-        return `
-        <svg height="100" width="100">
-            <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
-            Sorry, your browser does not support inline SVG.
-        </svg>
-        `;
+const promptUser = () =>
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'logoName',
+                message: 'Please enter 1-3 letters for your logo name:',
+                validate: (logoName) => {
+                    if (logoName.length > 3) {
+                        console.log('Please enter 1-3 letters for your logo name:');
+                        return false;
+                    }
+                    return true;
+                }
+            },
+            {
+                type: 'input',
+                name: 'logoColor',
+                message: 'Please enter a color for your logo text:',
+                default: 'white',
+            },
+            {
+                type: 'input',
+                name: 'logoTextColor',
+                message: 'Please enter a color for your logo text:',
+                default: 'black',
+            },
+            {
+                type: 'input',
+                name: 'logoBackgroundColor',
+                message: 'Please enter a color for your logo background:',
+                default: 'black',
+            },
+            {
+                type: 'list',
+                name: 'logoShape',
+                message: 'Please select a shape for your logo:',
+                choices: ['Square', 'Circle', 'Triangle', 'Star', 'Heart', 'Diamond', 'Rectangle'],
+                default: 'Square',
+            },
+        ])
+        .then((data) => {
+            const svgPath = "./dist/logo.svg";
+            const finalLogo = generateShape(data);
+
+            //Generate the svg logo here.
+            fs.writeFile(svgPath, generateSVG(finalLogo), (err) =>
+                err ? console.error(err) : console.log("Generated logo.svg")
+            );
+        })
+        .catch((err) => console.error(err));
+        
+const generateShape = (data) => {
+    const logoShape = data.logoShape;
+    switch (logoShape) {
+        case 'Square':
+            return generateSquareLogo(data);
+        case 'Circle':
+            return generateCircleLogo(data);
+        case 'Triangle':
+            return generateTriangleLogo(data);
+        case 'Star':
+            return generateStarLogo(data);
+        case 'Heart':
+            return generateHeartLogo(data);
+        case 'Diamond':
+            return generateDiamondLogo(data);
+        case 'Rectangle':
+            return generateRectangleLogo(data);
+        default:
+            return generateSquareLogo(data);
     }
 }
 
+const generateSquareLogo = (data) => {
+    const logoName = data.logoName;
+    const logoColor = data.logoColor;
+    const logoTextColor = data.logoTextColor;
+    const logoBackgroundColor = data.logoBackgroundColor;
+    const logoShape = data.logoShape;
+    return `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid meet">
+    <title>${logoName}</title>
+    <rect x="0" y="0" width="100" height="100" fill="${logoColor}" stroke="${logoTextColor}" stroke
+    ="none" />
+    </svg>`;
+}
 
+const generateCircleLogo = (data) => {
+    const logoName = data.logoName;
+    const logoColor = data.logoColor;
+    const logoTextColor = data.logoTextColor;
+    const logoBackgroundColor = data.logoBackgroundColor;
+    const logoShape = data.logoShape;
+    return `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid meet">
+    <title>${logoName}</title>
+    <circle cx="50" cy="50" r="40" fill="${logoColor}" stroke="${logoTextColor}" stroke
+    ="none" />
+    </svg>`;
+}
 
-const promptUser = () =>
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'logoName',
-            message: 'Please enter 1-3 letters for your logo name:',
-            validate: (logoName) => {
-                if (logoName.length > 3) {
-                    console.log('Please enter 1-3 letters for your logo name:');
-                    return false;
-                }
-                return true;
-            }
-        },
-        {
-            type: 'input',
-            name: 'logoTextColor',
-            message: 'Please enter a color for your logo text:',
-            validate: (logoTextColor) => {
-                if (logoTextColor.length === 0) {
-                    console.log('Please enter a color for your logo text:');
-                    return false;
-                }
-                return true;
-            }
-        },
-        {
-            type: 'input',
-            name: 'logoBackgroundColor',
-            message: 'Please enter a color for your logo background:',
-            validate: (logoBackgroundColor) => {
-                if (logoBackgroundColor.length === 0) {
-                    console.log('Please enter a color for your logo background:');
-                    return false;
-                }
-                return true;
-            }
-        },
-        {
-            type: 'list',
-            name: 'logoShape',
-            message: 'Please select a shape for your logo:',
-            choices: ['Square', 'Circle', 'Triangle', 'Star', 'Heart', 'Diamond', 'Rectangle' ]
-        },
-    ])
-    .then((answers) => {
-        const shape = generateShape(answers);
-        const svg = generateSVG(shape);
-        fs.writeFile('logo.html', svg, (err) => {
-            if (err) throw err;
-            console.log('The file has been saved!');
-        });
-    })
-    .catch((error) => {
-        if (error.isTtyError) {
-            console.log('Prompt could not be rendered in the current environment');
-        } else {
-            console.log('Something else went wrong');
-        }
-    });
+const generateTriangleLogo = (data) => {
+    const logoName = data.logoName;
+    const logoColor = data.logoColor;
+    const logoTextColor = data.logoTextColor;
+    const logoBackgroundColor = data.logoBackgroundColor;
+    const logoShape = data.logoShape;
+    return `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid meet">
+    <title>${logoName}</title>
+    <polygon points="50,0 0,50 50,100" fill="${logoColor}" stroke="${logoTextColor}" stroke
+    ="none" />
+    </svg>`;
+}
 
+const generateStarLogo = (data) => {
+    const logoName = data.logoName;
+    const logoColor = data.logoColor;
+    const logoTextColor = data.logoTextColor;
+    const logoBackgroundColor = data.logoBackgroundColor;
+    const logoShape = data.logoShape;
+    return `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid meet">
+    <title>${logoName}</title>
+    <polygon points="50,0 0,50 50,100" fill="${logoColor}" stroke="${logoTextColor}" stroke
+    ="none" />
+    </svg>`;
+}
 
+const generateHeartLogo = (data) => {
+    const logoName = data.logoName;
+    const logoColor = data.logoColor;
+    const logoTextColor = data.logoTextColor;
+    const logoBackgroundColor = data.logoBackgroundColor;
+    const logoShape = data.logoShape;
+    return `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid meet">
+    <title>${logoName}</title>
+    <polygon points="50,0 0,50 50,100" fill="${logoColor}" stroke="${logoTextColor}" stroke
+    ="none" />
+    </svg>`;
+}
+
+const generateDiamondLogo = (data) => {
+    const logoName = data.logoName;
+    const logoColor = data.logoColor;
+    const logoTextColor = data.logoTextColor;
+    const logoBackgroundColor = data.logoBackgroundColor;
+    const logoShape = data.logoShape;
+    return `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid meet">
+    <title>${logoName}</title>
+    <polygon points="50,0 0,50 50,100" fill="${logoColor}" stroke="${logoTextColor}" stroke
+    ="none" />
+    </svg>`;
+}
+
+const generateRectangleLogo = (data) => {
+    const logoName = data.logoName;
+    const logoColor = data.logoColor;
+    const logoTextColor = data.logoTextColor;
+    const logoBackgroundColor = data.logoBackgroundColor;
+    const logoShape = data.logoShape;
+    return `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid meet">
+    <title>${logoName}</title> <rect x="0" y="0" width="100" height="10 0" fill="${logoColor}" stroke="${logoTextColor}" stroke = "none" /> </svg>`;
+    }
+
+const generateSVG = (logo) => {
+    return `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid meet">
+    <title>${logo.logoName}</title>
+    <rect x="0" y="0" width="100" height="100" fill="${logo.logoColor}" stroke="${logo.logoTextColor}" stroke
+    ="none" />
+    </svg>`;
+}
 
 promptUser();
